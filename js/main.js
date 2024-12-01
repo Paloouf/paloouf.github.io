db3cd810e36564ace6493b0cb2da5b0156242d3a
Paloouf <paloouf@gmail.com>
2024-12-01 14:37:51 +0100

    fixed most of frontend, need to put backend for email service, add projects carousel

--- js/main.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('scroll', () => {
-    const parallax = document.querySelector('.parallax');
-    const scrollPosition = window.scrollY;
-    parallax.style.backgroundPositionY = `${scrollPosition * 0.5}px`; // Adjust speed with multiplier
-});
